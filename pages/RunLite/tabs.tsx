c04d361c9ebb9804198b1d5a717ab6c2deff752c
nhenin <nicolas.henin@iohk.io>
2023-05-23 16:57:49 +0200

    Create a new contract from a Core Marlowe JSON file not involving merkleization

--- pages/RunLite/tabs.tsx
+++ pages/RunLite/tabs.tsx
@@ -1,25 +1,22 @@
 import React,  {  } from 'react'
-import { Menu } from 'semantic-ui-react'
+import { Icon, Menu } from 'semantic-ui-react'
 import { Tab } from 'semantic-ui-react'
 import { useWalletState } from '../Hooks/Wallet'
 import { About } from './about'
+import { NewContract } from './new'
 
 export const RunLiteTabs = () => { 
   let walletState = useWalletState();
   const panes = () => { switch (walletState.type) {
     case 'disconnected' : return panesDisconnected 
     case 'connecting'   : return panesConnecting
-    case 'connected'    : return panesConnected
+    case 'connected'    : return [{ menuItem: <Menu.Item key='new'><Icon name='add circle' color='grey' size='large' /> New Contract </Menu.Item>,
+                                    render: () => <NewContract state={walletState}/>}]
   }}  
 
   return (<div><Tab panes={panes()} /></div>)
   
-   
 } 
-
-const panesConnected = [{
-  menuItem: <Menu.Item key='About'>About</Menu.Item>,
-  render: () => <About/>,}]
 
 const panesDisconnected = [{
       menuItem: <Menu.Item key='About'>About</Menu.Item>,
